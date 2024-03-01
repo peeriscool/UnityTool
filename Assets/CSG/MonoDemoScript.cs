a1c194606440db8a8c7b55f84212dba159731eb8
peeriscool <44834355+peeriscool@users.noreply.github.com>
2024-03-01 15:51:39 +0100

    Added CSG Booleans
    from  https://github.com/karl-/pb_CSG/tree/master/CSG/Classes

---
+++ Assets/CSG/MonoDemoScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Parabox.CSG;
+
+    public class MonoDemoScript : MonoBehaviour
+    {
+    //   CSG.BooleanOp intersect = CSG.BooleanOp.Intersection;
+    private void Start()
+    {
+        // Initialize two new meshes in the scene
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = Vector3.one * 1.3f;
+
+        // Perform boolean operation
+        Model result = CSG.Subtract(cube, sphere);
+
+        // Create a gameObject to render the result
+        var composite = new GameObject();
+        composite.AddComponent<MeshFilter>().sharedMesh = result.mesh;
+        composite.AddComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+
+
+        composite
+    }
+}
+

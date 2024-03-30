dc92c6784bc10feee84b4ce10d38242184ef1045
peeriscool <44834355+peeriscool@users.noreply.github.com>
2024-03-30 17:25:08 +0100

    updated history tab

--- Assets/Scripts/Uiscripts/UISettingsHandler.cs
+++ Assets/Scripts/Uiscripts/UISettingsHandler.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Linq;
 public class UISettingsHandler 
 {
     public UIDocument uiSettings ;
-    VisualElement myelement;    
+    VisualElement myelement;
+    DropdownField Historystack;
+    List<ICommand> history;
     public UISettingsHandler(UIDocument parent)
     {
         uiSettings = parent;
@@ -14,15 +17,13 @@ public class UISettingsHandler
     {
         myelement = new VisualElement();
         myelement.style.width = 400;
-        myelement.style.height = 1080;
+        myelement.style.height = 400;
         myelement.style.backgroundColor = Color.clear;
         Debug.Log(Resources.Load<Sprite>("Ui/ControlsMenu.jpg"));
         myelement.style.backgroundImage = Resources.Load<Texture2D>("Ui/ControlsMenu.jpg");
-        myelement.style.backgroundSize = new StyleBackgroundSize(StyleKeyword.Auto);
+        myelement.style.backgroundSize = new StyleBackgroundSize(StyleKeyword.Initial);
         uiSettings.panelSettings = Resources.Load<PanelSettings>("PanelSettings");
-        myelement.style.width = 300;
-        myelement.style.height = 300;
-        myelement.style.color = Color.red;
+  
     }
     public void LoadUi()
     {
@@ -32,16 +33,31 @@ public class UISettingsHandler
     }
     public void UpdateHistroy()
     {
-        Stack<ICommand> history = CommandInvoker.GetUndoStack();
+        history = CommandInvoker.GetUndoStack().ToList();
         List<string> options = new List<string>();
         for (int i = 0; i < history.Count; i++)
         {
             ICommand com = CommandInvoker.GetFromStack(i);
             options.Add(com.ToString()+ i.ToString());
         }
-        DropdownField Historystack = new DropdownField("History", options, 0);
-        uiSettings.rootVisualElement.Add(Historystack);
+         Historystack = new DropdownField("History", options, 0);
+        Historystack.RegisterValueChangedCallback(handlehistory);
+        myelement.Add(Historystack);
         Debug.Log("History: " + history.Count);
     }
-
+    private void handlehistory(ChangeEvent<string> evt)
+    {
+        Debug.Log($"The value has changed to {evt.newValue}.");
+        for (int i = 0; i < history.Count; i++)
+        {
+            if(evt.newValue == history.ElementAt(i) + i.ToString()) //if selected value equals to the type of command specified
+            {
+                Debug.Log("Selected "+history.ElementAt(i) +" from history: Undo");
+                CommandInvoker.GetFromStack(i).Undo();
+            //    history.RemoveAt(i);
+            //    Historystack.RemoveAt(i);
+            }
+        }
+        myelement.Q<DropdownField>("History");
+    }
 }
